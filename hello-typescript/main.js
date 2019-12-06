7d195d19ee25ddd845c6c489c3e80a8775f0fae3
ivens <ivensaugusto@hotmail.com>
2019-12-06 09:19:14 -0300

    curso de angular

---
+++ hello-typescript/main.js
@@ -0,0 +1,8 @@
+var minhaVar = 'minha variavel';
+var num2 = 2;
+var pi2 = 3.14;
+var numeros = [2, 3, 4, 1, , 5, 2, 8];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
---
+++ hello-typescript/main.ts
@@ -0,0 +1,15 @@
+
+var minhaVar = 'minha variavel';
+
+
+let num2 = 2;
+const pi2 = 3.14;
+
+
+var numeros = [2, 3, 4, 1, , 5, 2, 8];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+
+numeros.map(valor=>valor*2);
+
